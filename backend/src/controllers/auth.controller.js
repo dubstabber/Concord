ffa478e5629993032c9990464ccd72b048d9e4f3
dubstabber <taesan@protonmail.com>
2025-05-26 14:36:55 +0200

    Implement signup,auth and JWT in backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,51 @@
-export const signup = (req, res) => {
-  res.send("signup route");
+import bcryptjs from "bcryptjs";
+
+import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+  const { email, fullName, password } = req.body;
+  try {
+    if (!email || !fullName || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      email,
+      fullName,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+      res.status(201).json({
+        _id: newUser,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+        message: "User created successfully",
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller:", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const login = (req, res) => {
