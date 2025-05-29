8c7403d7dc0240d59e7716bddb730c9fe60bc2e1
dubstabber <taesan@protonmail.com>
2025-05-29 20:15:55 +0200

    Implement socket.io, receive messages in real-time, user online status + optimizations

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,7 @@ import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
@@ -16,7 +17,6 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-const app = express();
 
 const PORT = process.env.PORT || 5001;
 
@@ -32,7 +32,7 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on PORT:${PORT}`);
   connectDB();
 });
